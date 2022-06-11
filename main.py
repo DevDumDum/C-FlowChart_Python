ad6fd32a6db16bddd884ec742367e3a24cfe2812
MakuAren <69324542+MakuAren@users.noreply.github.com>
2022-06-11 09:47:48 +0800

    mapped row and column

--- main.py
+++ main.py
@@ -25,11 +25,10 @@ text.config(yscrollcommand=v.set)
 v.grid(column=1, row=1, sticky='NS')
 #======================================================
 
-def if_condition(res, i, if_bracketStart, if_bracketEnd, func):
+def condition(res, i, bracketStart, bracketEnd, func):
     temp_cond = "" #text to be placed inside the shape
     parenthesis1Count = 0 #para makuha parin as string yung mga may parenthesis sa loob ng condition
     parenthesis2Count = 0 #para makuha parin as string yung mga may parenthesis sa loob ng condition
-    print("Shape = Diamond")
     while i < len(res):
         status = 2
         if str(res[i]) == " " or str(res[i]) == "(":
@@ -57,96 +56,285 @@ def if_condition(res, i, if_bracketStart, if_bracketEnd, func):
         elif status == 0:
             break
 
-    print("Type = "+func[0][if_bracketStart])
-    print("Inside Shape = "+temp_cond)
+    func[3].append(temp_cond)
+    print("Type: "+func[0][len(func[0])-1])
+    print("Label: "+temp_cond)
     print("")
 
-    return i, if_bracketStart, if_bracketEnd
+    return i, bracketStart, bracketEnd, func
 
+def process(res, i, func):
+    process_label = ""
+
+    while res[i] != ";":
+        process_label += res[i]
+        i+=1
+    process_label +=res[i]
+    func[3].append(process_label)
+
+    print("Type: "+func[0][len(func[0])-1])
+    print("Label: "+process_label)
+    print("")
+
+    return i, func
+
+def position(func, tempBracketEnd):
+    func[2].append(tempBracketEnd)
+    return func
+
+def checker(res):
+    bracket = 0
+    parenthesis = 0
+    dqoute = 0
+    sqoute = 0
+    xx = 0
+    status = False
+    text = "Incomplete Code"
+    color = "red"
+    while xx < len(res):
+        if res[xx] == "{":
+            bracket+=1
+        elif res[xx] == "}":
+            bracket-=1
+        elif res[xx] == "(":
+            parenthesis+=1
+        elif res[xx] == ")":
+            parenthesis-=1
+        elif res[xx] == '"':
+            dqoute+=1
+        elif res[xx] == "'":
+            sqoute+=1
+        xx+=1
+    if bracket==0 and parenthesis==0 and dqoute%2!=1 and sqoute%2!=1:
+        status = True
+        text = "Converted"
+        color = "green"
+    else:
+        print(bracket)
+        print(parenthesis)
+        print(parenthesis)
+        print(dqoute%2)
+        print(sqoute%2)
+
+    return status, text, color
+
+def optimize_process_function(func):
+    x = 0
+    total = len(func[0])-1
+    while x <= total:
+        if x+1 < total:
+            if (func[0][x] == "process" or func[0][x] == "input" or func[0][x] == "output") and func[0][x] == func[0][x+1]:
+                func[0].pop(x+1)
+                func[2].pop(x+1)
+                func[3][x] += "\n"+func[3][x+1]
+                func[3].pop(x+1)
+        x+=1
+    return func
+
+def mapping(func, flowchart):
+    flowchart.append([])
+    total = len(func[2])
+    converted = []  #add in-between per shape for arrows
+    x = 0
+    tempn = 0
+    total_column = 0
+    total_row = 0
+    current_row = 0
+    last_column = 0
+    last_row = 0
+    left = 0
+
+    #get max column
+    while x < total:
+        converted.append(func[2][x]*2)
+
+        if func[2][x] > total_column:
+            total_column = func[2][x]
+        x+=1
+    total_column *= 2
+    textt=""
+    for i in range(len(func[0])):
+        textt += func[0][i][0]
+    
+    #converted[x] = column location
+    #flowchart[r][c]
+    x = 0 
+    cx = 0#index for converted[]
+    while x < total:
+        current_row = last_row
+
+        #check row
+        zz = len(flowchart)-1
+        if total_row > zz:
+            while total_row > zz:
+                flowchart.append([])
+                zz = len(flowchart)-1
+        #check column
+        zz = len(flowchart[current_row])-1
+        if converted[cx] > zz:
+            while converted[cx] > zz:
+                flowchart[current_row].append(None)
+                zz = len(flowchart[current_row])-1
+
+        flowchart[current_row][converted[cx]] = func[0][x][0]+func[0][x][1]
+        #print(flowchart[current_row])
+
+        if cx+1<total:
+            if converted[cx] == converted[cx+1]-2:
+                flowchart[current_row].append(">")
+            else:
+                total_row+=1
+                current_row+=1
+        last_row = current_row
+        cx+=1
+        x+=1
+        
+    print("=========================")
+    print("total row: "+str(total_row))
+    print("Position: "+str(converted))
+    print("")
+    for z in range(len(flowchart)):
+        print(flowchart[z])
+    return 0
 
 # Button
-def clicked():
+def convertBtn():
     res = text.get("1.0",'end-1c')
-    text_code_count = len(res)
-    if_bracketStart = 0
-    if_bracketEnd = 0
-    total_if = 0
-    total_else_if = 0
-    
-    func = []
-    func.append([])
-    func.append([])
+    bt["text"] = str("Checking")
+    status, tempText, BtnColor = checker(res)
+    bt["text"] = str(tempText)
+    bt["bg"] = BtnColor
+    if status == True:
+        text_code_count = len(res)
 
-    i = 0
-    print('total characters = '+str(text_code_count))
-    print("")
-    while (text_code_count > i ):
-        if(text_code_count > i+6): #check if posible pa magka else if
-            if str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4])+str(res[i+5])+str(res[i+6]) == "else if":
-                func[0].append("else if")
-                temp = 0
-                if if_bracketStart != if_bracketEnd:
-                    x = 0
-                    while (x < len(func[0])): #search for first if
-                        if func[0][x] == "if":
-                            temp = x
-                        x+=1
-                    func[1].append(temp)
-                else:
-                    x = 0
-                    while (x < len(func[0])): #search for first if
-                        if func[0][x] == "if":
-                            temp = x
-                            break
-                        x+=1
-                    func[1].append(temp)
-                total_else_if += 1
-                i+=7
-                print("--------------------")
-                print(str(if_bracketStart) + ":" + str(if_bracketEnd))
-                i, if_bracketStart, if_bracketEnd = if_condition(res, i, if_bracketStart, if_bracketEnd, func)
-        # if(text_code_count > i+5):
-        #     print("")
-        # if(text_code_count > i+4):
-        #     print("")
-
-        # if(text_code_count > i+3):
-        #     print("")
-
-        # if(text_code_count > i+2):
-        #     print("")
+        bracketStart = 0
+        bracketEnd = 0
+        total_if = 0
+        total_else_if = 0
+        tempBracketEnd = 0
         
+        func = []
+        func.append([]) #label              0
+        func.append([]) #bracket status     1
+        func.append([]) #position           2
+        func.append([]) #text_label         3
 
-        if(text_code_count > i+1): #check if posible pa magka if
-            if  str(res[i])+str(res[i+1]) == "if":
-                func[0].append("if")
-                if len(func[1]) == 0:
-                    func[1].append(None)
-                else:
-                    func[1].append(if_bracketStart-1)
-                total_if+=1
-                print(total_if)
-                i+=2
+        flowchart = []  #map of flowchart
+
+        i = 0
+        print("")
+        print('total characters = '+str(text_code_count))
+        print("")
+        while (text_code_count > i ):
+            condStatus = 0
+            if(text_code_count > i+7): #check if posible pa magka else if
+                if str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4])+str(res[i+5])+str(res[i+6]) == "else if":
+                    conType = "else if"
+                    i+=7
+                    condStatus = 1
+
+            if(text_code_count > i+6):
+                if str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4])+str(res[i+5]) == "double" and str(res[i+6]) == " ":
+                    conType = "process"
+                    condStatus = 1
+                elif str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4])+str(res[i+5]) == "printf":
+                    conType = "output"
+                    condStatus = 1
+
+            if(text_code_count > i+5):
+                if str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4]) == "while":
+                    conType = "while"
+                    condStatus = 1
+                    i+=5
+                elif str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4]) == "float" and str(res[i+5]) == " ":
+                    conType = "process"
+                    condStatus = 1
+                elif str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3])+str(res[i+4]) == "scanf":
+                    conType = "input"
+                    condStatus = 1
+
+            if(text_code_count > i+4):
+                if str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3]) == "else":
+                    conType = "else"
+                    i+=5
+                    condStatus = 1
+                elif str(res[i])+str(res[i+1])+str(res[i+2])+str(res[i+3]) == "char" and str(res[i+4]) == " ":
+                    conType = "process"
+                    condStatus = 1
+
+            if(text_code_count > i+3):
+                if str(res[i])+str(res[i+1])+str(res[i+2]) == "int" and str(res[i+3]) == " ":
+                    conType = "process"
+                    condStatus = 1
+
+            # if(text_code_count > i+2):
+            #     print("")
+
+            if(text_code_count > i+1): #check if posible pa magka if
+                if  str(res[i])+str(res[i+1]) == "if":
+                    conType = "if"
+                    i+=2
+                    condStatus = 1
+            
+            if condStatus == 1:
                 print("--------------------")
-                i, if_bracketStart, if_bracketEnd = if_condition(res, i, if_bracketStart, if_bracketEnd, func)
-        if(text_code_count > i ):
-            if res[i] == "{":
-                if_bracketStart+=1
+                func[0].append(conType)
+
+                if conType == "else if" or conType == "if" or conType == "while" or conType == "for":
+                    print("Shape: Diamond")
+                    i, bracketStart, bracketEnd, func = condition(res, i, bracketStart, bracketEnd, func)
+                elif conType == "process":
+                    print("Shape: Rectangle")
+                    i, func = process(res, i, func)
+                elif conType == "input" or conType == "output":
+                    print("Shape: Parrallelogram")
+                    i, func = process(res, i, func)
+                elif conType == "else":
+                    print("else arrow")
+                    func[3].append("else")
+
+                
+                func = position(func, tempBracketEnd)
+
+            if text_code_count > i:
+                if res[i] == "{":
+                    bracketStart+=1
+                    tempBracketEnd += 1
+                    
+                elif res[i] == "}":
+                    # xx = len(func[1])-1
+                    # while xx >= 0:
+                    #     if func[1][xx] == 1:
+                    #         func[1][xx] = 0
+                    #         break
+                    #     xx-=1
+                    # bracketEnd+=1
+                    tempBracketEnd -= 1
                 i+=1
-            elif res[i] == "}":
-                if_bracketEnd+=1
-                i+=1
-            elif res[i] == " ":
-                i+=1
-            elif res[i] != "":
-                i+=1
-    print(func[0])
-    print(func[1])
-    print(str(if_bracketStart) + ":" + str(if_bracketEnd))
+        func = optimize_process_function(func)
+        flowchart = mapping(func, flowchart)
+        print("--------------------")
+        # print("Label: "+str(func[0]))
+        # #print("Status: "+str(func[1]))
+        # print("Position: "+str(func[2]))
+        # print("Content: "+str(func[3]))
+
+        #dapat nagawa na yung pang function para ma uncomment ito
+        # if len(func[2]) != bracketStart/2:
+        #     bt["text"] = "statement error"
+        #     bt["bg"] = "red"
+        #     print(len(func[2]))
+        #     print(bracketStart)
+    else:
+        print(tempText)
 
 
+bt = tk.Button(root,
+            text="Convert",
+            bg="green",
+            fg="white",
+            command=convertBtn)
 
-bt = tk.Button(root,text="Convert",bg="green",fg="white",command=clicked)
 bt.grid(column=0,row=2)
 
 #======================================================
